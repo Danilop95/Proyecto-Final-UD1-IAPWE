377b0502b63c069e29e23918d8ab42d9c4820ff0
DVNILXP <59531081+Danilop95@users.noreply.github.com>
2024-11-20 22:55:53 +0000

    Refactor conexión a la base de datos para simplificar la creación del objeto PDO

--- src/db.php
+++ src/db.php
@@ -1,20 +1,11 @@
 <?php
-$host = getenv('MYSQL_HOST');
-$db   = getenv('MYSQL_DATABASE');
-$user = getenv('MYSQL_USER');
-$pass = getenv('MYSQL_PASSWORD');
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
+    $pdo = new PDO(
+        "mysql:host=" . getenv('MYSQL_HOST') . ";dbname=" . getenv('MYSQL_DATABASE') . ";charset=utf8mb4",
+        getenv('MYSQL_USER'),
+        getenv('MYSQL_PASSWORD')
+    );
+} catch (PDOException $e) {
     die("Error en la conexión: " . $e->getMessage());
 }
 ?>
